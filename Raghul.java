2d7c6768afb9fc2be6975175dff40f22eee6c784
RaghulKannan007 <raghulkannan92@gmail.com>
2021-12-22 21:55:20 +0530

    pushtoscanner

---
+++ Raghul.java
@@ -0,0 +1,26 @@
+package com.Scanner1;
+
+import java.util.Scanner;
+
+public class Raghul {
+public static void main(String[] args) {
+	Scanner Go = new Scanner(System.in);
+	
+	
+	System.out.println("Tamil");
+		int a = Go.nextInt();
+	System.out.println(a);
+	
+	System.out.println("English");
+	int b = Go.nextInt();
+	System.out.println(b);
+	
+	System.out.println("Average marks");
+	int c = a + b;
+	int d = c/2;
+	System.out.println(d);
+	
+	
+	
+}
+}
---
+++ Scanner2.java
@@ -0,0 +1,14 @@
+package com.Scanner1;
+
+import java.util.Scanner;
+
+public class Scanner2 {
+
+	public static void main(String[] args) {
+		Scanner Raghul = new Scanner(System.in);
+		
+		System.out.println("Universe loves me");
+		char Raghul1 = '$';
+		
+	}
+}
